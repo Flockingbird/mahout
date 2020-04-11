eca8f819c163cb198851ef454af9be3eae53b8d9
Bèr Kessels <ber@berk.es>
2020-04-11 14:53:06 +0200

    Render the professionals using model attributes and partials

--- app/controllers/professionals_controller.rb
+++ app/controllers/professionals_controller.rb
@@ -1,5 +1,17 @@
 class ProfessionalsController < ApplicationController
   layout "public"
+
   def index
+    @professionals = find_resources
+  end
+
+  private
+
+  def resource_scope
+    Profile.all
+  end
+
+  def find_resources
+    resource_scope
   end
 end
