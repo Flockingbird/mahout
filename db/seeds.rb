571cfd31ea7f964515da1c25280bb97405144fe3
Bèr Kessels <ber@berk.es>
2020-04-11 15:15:04 +0200

    Seed the database with fake profiles

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,7 @@
+# frozen_string_literal: true
+
+require 'csv'
+
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
 #
@@ -5,3 +9,13 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+
+# Profiles
+seed_csv = Rails.root.join('db', 'seeds', 'profiles.csv')
+model_class = Profile
+records = []
+CSV.foreach(seed_csv, headers: true) do |row|
+  records << model_class.find_or_create_by(row.to_h)
+end
+puts "Imported #{records.count} #{model_class} records"
