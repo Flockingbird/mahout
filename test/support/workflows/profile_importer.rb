22272acf994fd9a4c482dbc4b7c585e690d776b4
Bèr Kessels <ber@berk.es>
2020-04-12 15:15:37 +0200

    Add Workflow helpers for tests

---
+++ test/support/workflows/profile_importer.rb
@@ -0,0 +1,24 @@
+module Workflows
+  require "csv"
+  # Imports profiles from a CSV
+  class ProfileImporter
+    def initialize(count: 1)
+      @count = count
+    end
+
+    def call
+      seed_csv = Rails.root.join('db', 'seeds', 'profiles.csv')
+      model_class = Profile
+
+      records = []
+      amt_created = 0
+      CSV.foreach(seed_csv, headers: true) do |row|
+        records << model_class.create!(row.to_h)
+        amt_created += 1
+        break if amt_created >= @count
+      end
+
+      records
+    end
+  end
+end
--- test/test_helper.rb
+++ test/test_helper.rb
@@ -2,6 +2,12 @@ ENV['RAILS_ENV'] ||= 'test'
 require_relative '../config/environment'
 require 'rails/test_help'
 
+require 'support/workflows.rb'
+glb = Rails.root.join('test', 'support', '**', '*rb')
+Dir[glb].each do |file|
+  require file
+end
+
 class ActiveSupport::TestCase
   # Run tests in parallel with specified workers
   parallelize(workers: :number_of_processors)
