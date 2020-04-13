aea2feacad25fea83f161ef2d66e5d688f1dc63e
Bèr Kessels <ber@berk.es>
2020-04-13 16:09:57 +0200

    Silence puma when starting in tests

--- test/application_system_test_case.rb
+++ test/application_system_test_case.rb
@@ -2,4 +2,7 @@ require "test_helper"
 
 class ApplicationSystemTestCase < ActionDispatch::SystemTestCase
   driven_by :selenium, using: :headless_firefox, screen_size: [1400, 1400]
+  setup do
+    Capybara.server = :puma, { Silent: true }
+  end
 end
