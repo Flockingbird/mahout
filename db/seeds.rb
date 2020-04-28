dac67ad76eba895c82377e8b7d0cd8eaa99e4fb9
Bèr Kessels <ber@berk.es>
2020-04-28 20:32:28 +0200

    Merge contact details from sample csv into profiles

--- db/seeds.rb
+++ db/seeds.rb
@@ -19,8 +19,32 @@ header_images = Dir[Rails.root.join('db', 'seeds', 'headers', '*.jpg')]
 
 model_class = Profile
 records = []
+
+contact_lines = CSV.read(Rails.root.join('db', 'seeds', 'contact_details.csv'), headers: true).to_enum
+
 CSV.foreach(seed_csv, headers: true) do |row|
-  record = model_class.find_or_initialize_by(row.to_h)
+  contact_details = []
+  contact_lines.next.each do |key, value|
+    next unless key && value
+    type = case key
+           when "Home", "Work"
+             :phone
+           when "Personal", "Workemail"
+             :email
+           when "Twitter"
+             :twitter
+           when "Facebook"
+             :facebook
+           when "Linkedin"
+             :linkedin
+           when "Street Address"
+             :address
+           end
+    contact_details <<  { key: key, value: value, type: type }
+  end
+
+  attrs = row.to_h.merge(contact_details: contact_details)
+  record = model_class.find_or_initialize_by(attrs)
   # randomly add avatar
   if (rand > 0.3)
     file = avatar_files.sample
@@ -34,7 +58,6 @@ CSV.foreach(seed_csv, headers: true) do |row|
   end
 
   record.save!
-
   records << record
 end
 puts "Imported #{records.count} #{model_class} records"
