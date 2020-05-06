f9a6de60d7b99f330b82ca5c377b69f0d80d4853
Bèr Kessels <ber@berk.es>
2020-05-06 16:30:00 +0200

    Allow local dev env to be served through ngrok

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -63,4 +63,7 @@ Rails.application.configure do
   # Use an evented file watcher to asynchronously detect changes in source code,
   # routes, locales, etc. This feature depends on the listen gem.
   config.file_watcher = ActiveSupport::EventedFileUpdateChecker
+
+  # Allow ngrok to access dev
+  config.hosts << '98733280.ngrok.io'
 end
